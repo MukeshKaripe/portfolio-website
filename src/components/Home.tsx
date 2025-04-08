f706db1502a85273f8cc1070103c11eb2dfd05a1
MukeshKaripe <karipemukesh@gmail.com>
2025-04-08 17:15:13 +0530

    updated resume

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -128,7 +128,7 @@ const Home: React.FC = () => {
           <a
             target="_blank"
             rel="noopener noreferrer"
-            href="src/assets/Mukesh_Karipe(Resume).pdf"
+            href="/Mukesh_Karipe(Resume).pdf"
             className="px-8 py-3 block font-edu w-fit text-white font-medium text-lg rounded-lg shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-4"
             style={{
               background: "linear-gradient(to right, var(--accent-color), var(--accent-color-alt))",
